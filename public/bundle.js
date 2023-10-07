f48191201b60e6802714e733551df4cc8160e4c8
Ramon Estadella Serra <144245823+RamonDataScience@users.noreply.github.com>
2023-10-07 17:20:27 +0200

    thumbs y colores producto

--- public/bundle.js
+++ public/bundle.js
@@ -1,11 +1,38 @@
 'use strict';
 
+// VARIABLES
+
 const producto = document.getElementById('producto');
-producto.querySelector('.producto__imagen');
+const productoImagen = producto.querySelector('.producto__imagen');
 const thumbs = producto.querySelector('.producto__thumbs');
+
+// Color
+
+const propiedadColor = producto.querySelector('#propiedad-color');
+
+
+// THUMBS DEL PRODUCTO
 
 thumbs.addEventListener('click', (e) => {
     if(e.target.tagName === 'IMG') {
-        e.target.src;
+        const imagenSrc = e.target.src;
+
+         // Obtenemos la posicion del ultimo /
+        const lastIndex = imagenSrc.lastIndexOf('/');
+
+        // Cortamos la cadena de texto para obtener solamente una parte
+        const nombreImagen = imagenSrc.substring(lastIndex + 1);
+
+        // Cambiamos la ruta de la imagen del producto
+        productoImagen.src = `./img/tennis/${nombreImagen}`;
+    }
+    
+});
+
+// COLORES DEL PRODUCTO
+
+propiedadColor.addEventListener('click', (e) => {
+    if (e.target.tagName === 'INPUT') {
+        productoImagen.src = `./img/tennis/${e.target.value}.jpg`;
     }
 });
