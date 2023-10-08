b3cb9b4f2de62b53aaf8ac4ebbea2cb5c557d9e8
Ramon Estadella Serra <144245823+RamonDataScience@users.noreply.github.com>
2023-10-08 10:14:34 +0200

    cantidad del producto

--- public/bundle.js
+++ public/bundle.js
@@ -1,19 +1,21 @@
 'use strict';
 
 // VARIABLES
-
 const producto = document.getElementById('producto');
 const productoImagen = producto.querySelector('.producto__imagen');
 const thumbs = producto.querySelector('.producto__thumbs');
 
 // Color
-
 const propiedadColor = producto.querySelector('#propiedad-color');
+
+// Cantidad
+const btnDisminuirCantidad = producto.querySelector('#disminuir-cantidad');
+const btnIncrementarCantidad = producto.querySelector('#incrementar-cantidad');
+const inputCantidad = producto.querySelector('#cantidad');
 
 
 // THUMBS DEL PRODUCTO
 // La idea es cambiar la ruta de la foto cuando el usuario haga click.
-
 thumbs.addEventListener('click', (e) => {
     if(e.target.tagName === 'IMG') {
         const imagenSrc = e.target.src;
@@ -32,9 +34,19 @@ thumbs.addEventListener('click', (e) => {
 
 // COLORES DEL PRODUCTO
 // La idea es cambiar la ruta de la foto cuando el usuario haga click.
-
 propiedadColor.addEventListener('click', (e) => {
     if (e.target.tagName === 'INPUT') {
         productoImagen.src = `./img/tennis/${e.target.value}.jpg`;
+    }
+});
+
+// CANTIDAD DEL PRODUCTO
+btnIncrementarCantidad.addEventListener('click', (e) => {
+    inputCantidad.value = parseInt(inputCantidad.value) + 1;
+});
+
+btnDisminuirCantidad.addEventListener('click', (e) => {
+    if(parseInt(inputCantidad.value) > 1) {
+        inputCantidad.value = parseInt(inputCantidad.value) - 1;
     }
 });
